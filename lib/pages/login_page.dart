b24ef6923aee9b9616d6bdaa224e3a79e432565c
safiahmad <safiahmad129>
2024-07-26 04:37:01 -0700

    day 6 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:project_1/pages/home_page.dart';
 import 'package:project_1/utils/routes.dart';
 
-
 void main() {
   runApp(MaterialApp(
     initialRoute: '/',
@@ -16,29 +15,30 @@ void main() {
 class LoginPage extends StatefulWidget {
   @override
   State<LoginPage> createState() => _LoginPageState();
-  
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
   String name = "";
-  bool changebutton = false;
+  bool changeButton = false;
+
   @override
   Widget build(BuildContext context) {
     return Material(
       child: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.only(top: 60), // Add padding to push the image upwards
+            padding: const EdgeInsets.only(top: 30),
             child: SizedBox(
-              width: 300, // Set the desired width
-              height: 300, // Set the desired height
+              width: 300,
+              height: 300,
               child: Image.asset(
                 "assets/images/undraw_Loving_story_re_wo5x.png",
                 fit: BoxFit.contain,
               ),
             ),
           ),
-          Spacer(), // Adds space between the image and the text
+          Spacer(),
           Text(
             "WELCOME $name",
             style: TextStyle(
@@ -47,79 +47,86 @@ class _LoginPageState extends State<LoginPage> {
               color: const Color.fromARGB(255, 2, 96, 143),
             ),
           ),
-          SizedBox(height: 10), // Add some space below the welcome text
+          SizedBox(height: 0),
           Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min, // Centers the column within the available space
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 500.0, vertical: 10.0), // Adjust padding to fit the screen
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      hintText: "ENTER USERNAME",
-                      labelText: "USERNAME",
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 500.0, vertical: 10.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        hintText: "ENTER USERNAME",
+                        labelText: "USERNAME",
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "USERNAME CANNOT BE EMPTY";
+                        }
+                        return null;
+                      },
+                      onChanged: (value) {
+                        name = value;
+                        setState(() {});
+                      },
                     ),
-                    onChanged: (value) {
-                    name = value;
-                    setState(() {});
+                  ),
+                  SizedBox(height: 0),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 500.0, vertical: 10.0),
+                    child: TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: "ENTER PASSWORD",
+                        labelText: "PASSWORD",
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "PASSWORD CANNOT BE EMPTY";
+                        } else if (value.length < 6) {
+                          return "PASSWORD LENGTH SHOULD BE AT LEAST 6";
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  InkWell(
+                    onTap: () async {
+                      if (_formKey.currentState!.validate()) {
+                        setState(() {
+                          changeButton = true;
+                        });
+
+                        await Future.delayed(Duration(seconds: 1));
+                        await Navigator.pushNamed(context, MyRoutes.homeRoute);
+                        setState(() {
+                          changeButton = false;
+                        });
+                      }
                     },
-                    
-                  ),
-                ),
-                SizedBox(height: 20), // Add space between the text fields
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 500.0, vertical: 10.0), // Adjust padding to fit the screen
-                  child: TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: "ENTER PASSWORD",
-                      labelText: "PASSWORD",
-                    ),
-
-                  ),
-
-                ),
-                SizedBox(height: 20),
-
-                InkWell(
-                  onTap: () async{
-                    setState(() {
-                      changebutton = true;
-                    });
-                    await Future.delayed(Duration(seconds: 1));
-                    Navigator.pushNamed(context, MyRoutes.homeRoute);
-                  },
-                  child: AnimatedContainer(
-                    duration: Duration(seconds: 1 ),
-                    width:changebutton ? 50: 120,
-                    height: 40,
-                    alignment: Alignment.center,
-                    
-                    child: changebutton? Icon(Icons.done,color: Colors.white,): Text("LOGIN",
-                    style: TextStyle(color: Colors.white),
-                    
-                    ),
-                    decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(8)
-                  
-                  
+                    child: AnimatedContainer(
+                      duration: Duration(seconds: 1),
+                      width: changeButton ? 50 : 120,
+                      height: 40,
+                      alignment: Alignment.center,
+                      child: changeButton ? Icon(Icons.done, color: Colors.white) : Text(
+                        "LOGIN",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
                     ),
                   ),
-                )
-                
-
-
-                //ElevatedButton(
-                //  onPressed: () {
-                //    Navigator.pushNamed(context, MyRoutes.homeRoute);
-                //  },
-                //  child: Text("LOGIN"),
-                //),
-              ],
+                ],
+              ),
             ),
           ),
-          Spacer(), // Adds space at the bottom
+          Spacer(),
         ],
       ),
     );
